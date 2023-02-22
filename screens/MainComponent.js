dd55da8a96b87774a9afc3600fb38b60dfc48ef4
Dan Galuardi <dan.galuardi@gmail.com>
2023-02-22 17:12:40 -0500

    Home Screen and Drawer to Navigation

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,9 +1,32 @@
 import { View, Platform } from 'react-native';
 import Constants from 'expo-constants';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import AvailableAnimals from './AvailableAnimals';
 import AnimalInfoScreen from './AnimalInfoScreen';
 import AnimalTypesScreen from './AnimalTypesScreen';
+import HomeScreen from './HomeScreen';
+
+const Drawer = createDrawerNavigator();
+
+const screenOptions = {
+    headerTintColor: '#fff',
+    headerStyle: { backgroundColor: '#5637DD' }
+};
+
+const HomeNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return(
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Home'
+                component={HomeScreen}
+                options={{title: 'Home'}}
+            />
+        </Stack.Navigator>
+    )
+}
 
 const AnimalNavigator = () => {
     const Stack = createStackNavigator();
@@ -11,12 +34,7 @@ const AnimalNavigator = () => {
     return (
         <Stack.Navigator
             initialRouteName='Available Animals'
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: '#5637DD'
-                },
-                headerTintColor: '#fff'
-            }}
+            screenOptions={screenOptions}
         >
             <Stack.Screen
                 name='AnimalType'
@@ -51,7 +69,21 @@ const Main = () => {
                     Platform.OS ==='ios' ? 0 : Constants.statusBarHeight
             }}
         >
-            <AnimalNavigator />
+            <Drawer.Navigator
+                initialRouteName='Home'
+                drawerStyle={{ backgroundColor: '#CEC8FF' }}
+            >
+                <Drawer.Screen
+                    name='Home'
+                    component={HomeNavigator}
+                    options={{ title: 'Home' }}
+                />
+                <Drawer.Screen
+                    name='AvailableAnimals'
+                    component={AnimalNavigator}
+                    options={{ title: 'Available Animals' }}
+                />
+            </Drawer.Navigator>
         </View>
     );
 };
